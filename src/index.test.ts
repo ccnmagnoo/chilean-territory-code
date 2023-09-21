6a1770a74c86761eceedc14e5f2d23c0dd0c7b74
ccnmagnoo <crcamposn@gmail.com>
2023-09-21 10:37:58 -0300

    update test

--- src/index.test.ts
+++ src/index.test.ts
@@ -4,3 +4,6 @@ import getTerritoryInfo from '.';
 test('getting city info of valpariso as Valparaíso', () => {
   expect(getTerritoryInfo('valparaiso')?.name).toBe('Valparaíso');
 });
+test('region valpo as Valparaíso', () => {
+  expect(getTerritoryInfo('Quuillot', 'province')?.name).toBe('Quillota');
+});
